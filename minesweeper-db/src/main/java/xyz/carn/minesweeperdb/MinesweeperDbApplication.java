65aed146d48d94ff9a99dfd4d033d55c6d747358
Neil Carrie <neil.carrie@gmail.com>
2021-09-10 16:23:39 -0400

    Create springboot project

---
+++ minesweeper-db/src/main/java/xyz/carn/minesweeperdb/MinesweeperDbApplication.java
@@ -0,0 +1,13 @@
+package xyz.carn.minesweeperdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinesweeperDbApplication.class, args);
+    }
+
+}
