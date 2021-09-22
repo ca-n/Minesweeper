781a5d5ff30b974e83fe4775b22e3887221b1b4e
Neil Carrie <neil.carrie@gmail.com>
2021-09-22 00:17:22 -0400

    Create Board.js and Cell.js

---
+++ minesweeper-web/src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Board = ({board, cols, rows, state, onClick, onFlag}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Board
