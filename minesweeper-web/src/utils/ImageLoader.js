52e8f0940f209156687490e10af084bea2a796a8
Neil Carrie <neil.carrie@gmail.com>
2021-09-27 08:27:49 -0400

    Create images and add to ImageLoader

--- minesweeper-web/src/utils/ImageLoader.js
+++ minesweeper-web/src/utils/ImageLoader.js
@@ -1,7 +1,15 @@
 const images = {
-    mine: require('../logo.svg'),
-    flag: require('../logo.svg'),
-    flaggedMine: require('../logo.svg'),
+    one: require('../assets/one.png'),
+    two: require('../assets/two.png'),
+    three: require('../assets/three.png'),
+    four: require('../assets/four.png'),
+    five: require('../assets/five.png'), 
+    six: require('../assets/six.png'),
+    seven: require('../assets/seven.png'),
+    eight: require('../assets/eight.png'),
+    flag: require('../assets/flag.png'),
+    mine: require('../assets/mine.png'),
+    noMine: require('../assets/no_mine.png'),
     notFount: require('../logo.svg')
 };
 
