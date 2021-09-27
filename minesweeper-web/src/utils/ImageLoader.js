77ec423275a6af903e498b8159ea3d636fc75622
Neil Carrie <neil.carrie@gmail.com>
2021-09-27 09:38:42 -0400

    Fix problems with ImageLoader not loading images

--- minesweeper-web/src/utils/ImageLoader.js
+++ minesweeper-web/src/utils/ImageLoader.js
@@ -1,22 +1,35 @@
+import one from '../assets/one.png'
+import two from '../assets/two.png'
+import three from '../assets/three.png'
+import four from '../assets/four.png'
+import five from '../assets/five.png'
+import six from '../assets/six.png'
+import seven from '../assets/seven.png'
+import eight from '../assets/eight.png'
+import flag from '../assets/flag.png'
+import mine from '../assets/mine.png'
+import noMine from '../assets/no_mine.png'
+import notFound from '../assets/not_found.png'
+import none from '../assets/none.png'
+
 const images = {
-    one: require('../assets/one.png'),
-    two: require('../assets/two.png'),
-    three: require('../assets/three.png'),
-    four: require('../assets/four.png'),
-    five: require('../assets/five.png'), 
-    six: require('../assets/six.png'),
-    seven: require('../assets/seven.png'),
-    eight: require('../assets/eight.png'),
-    flag: require('../assets/flag.png'),
-    mine: require('../assets/mine.png'),
-    noMine: require('../assets/no_mine.png'),
-    notFound: require('../assets/not_found.png'),
-    none: require('../assets/none.png')
+    one: one,
+    two: two,
+    three: three,
+    four: four,
+    five: five, 
+    six: six,
+    seven: seven,
+    eight: eight,
+    flag: flag,
+    mine: mine,
+    noMine: noMine,
+    none: none,
+    notFound: notFound
 };
 
 export function loadImage(name) {
     if (images.hasOwnProperty(name)) {
-        console.log('Returned ' + JSON.stringify(images[name]));
         return images[name];
     }
     return images['notFound'];
