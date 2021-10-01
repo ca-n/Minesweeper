36273642d531cb5d5b1bc3ce9d2581adbea81a2b
Neil Carrie <neil.carrie@gmail.com>
2021-09-30 20:59:40 -0400

    Replace functions const arrow functions

--- minesweeper-web/src/components/cells/Empty.js
+++ minesweeper-web/src/components/cells/Empty.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Button from "../Button";
 const Empty = ({x, y, onClick, onFlag}) => {
 
-    function onLeftClick() {
+    const onLeftClick = () => {
         onClick(x, y);
     }
 
-    function onRightClick(){
+    const onRightClick = () => {
         onFlag(x, y);
     }
 
