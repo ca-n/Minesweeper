1dd5945baa4faef76a86f0fb37a38d82b4bb3b74
Neil Carrie <neil.carrie@gmail.com>
2021-09-30 20:58:02 -0400

    Fix onRightClick for Flag

--- minesweeper-web/src/components/cells/Flag.js
+++ minesweeper-web/src/components/cells/Flag.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { loadImage } from '../../utils/ImageLoader';
 import Button from "../Button";
 const Flag = ({x, y, onFlag}) => {
+
+    const onRightClick = () => {
+        onFlag(x, y)
+    }
+
     return (
         <Button image={'flag'}
                 background={'lightgray'}
-                onRightClick={onFlag} />
+                onRightClick={onRightClick} />
     )
 }
 
