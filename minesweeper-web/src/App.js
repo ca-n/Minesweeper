75c8706311726febdfa8c574b3db2492591561c5
Neil Carrie <neil.carrie@gmail.com>
2021-09-30 21:00:49 -0400

    Add parameters to Minesweeper and add test Cells

--- minesweeper-web/src/App.js
+++ minesweeper-web/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Minesweeper from "./components/Minesweeper";
+import Flag from './components/cells/Flag';
+import Mine from './components/cells/Mine';
+import NoMine from './components/cells/NoMine';
+import Revealed from './components/cells/Revealed';
 
 function App() {
   return (
     <div className="App">
-      <Minesweeper />
+      <Minesweeper cols={10} rows={10} mines={20}/>
+      <Flag />
+      <Mine />
+      <NoMine />
+      <Revealed adjacentMines='1' />
+      <Revealed adjacentMines='2' />
+      <Revealed adjacentMines='3' />
+      <Revealed adjacentMines='4' />
+      <Revealed adjacentMines='5' />
+      <Revealed adjacentMines='6' />
+      <Revealed adjacentMines='7' />
+      <Revealed adjacentMines='8' />
     </div>
   );
 }
